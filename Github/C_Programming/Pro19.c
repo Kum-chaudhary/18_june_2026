88c82e3b8624072323999d5ff20cb28d99219be8
Kum-chaudhary <kumkum.chaudhary@atsweb.onmicrosoft.com>
2026-07-01 18:29:28 +0530

    Add functionality to find the greatest of two numbers

--- Github/C_Programming/Pro19.c
+++ Github/C_Programming/Pro19.c
@@ -3,9 +3,18 @@
 void main()
 {
 	//variable declaration
+	int num1, num2;
+   int result = 0;
+
+    printf("enter the number");
+    scanf("%d",&num1);
+    printf("enter the number");
+    scanf("%d",&num2);
+
+     result = num1 > num2 ? num1 : num2 ;
+     printf("\n gretest number is %d", result);
 	
 	
 	
-	// variable initlization
 	
 }
